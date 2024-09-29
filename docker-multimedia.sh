279b2a386a583c57142866f1e2bae22e264f94df
Bensuperpc <bensuperpc@gmail.com>
2024-09-29 20:05:03 +0200

    Update scripts

--- docker-multimedia.sh
+++ docker-multimedia.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 set -euo pipefail
 
-# Run the docker container
-# --device=/dev/dri \
+REGISTRY="docker.io"
+IMAGE="bensuperpc/multimedia"
+TAG="archlinux-base"
+
+DOCKER_IMAGE="qt-multimedia-$(uuidgen)"
+
+CPUS="8.0"
+CPU_SHARES="1024"
+RES_RAM="2GB"
+MAX_RAM="16GB"
+TMP_RAM="4GB"
+
+PUID="$(id -u)"
+PGID="$(id -g)"
 
 docker run --rm -it \
-        --security-opt no-new-privileges --read-only --cap-drop SYS_ADMIN --user 1000:1000 \
+        --security-opt no-new-privileges --read-only --cap-drop SYS_ADMIN --user "$PUID:$PGID" \
         --mount type=bind,source=$(pwd),target=/work --workdir /work \
-        --mount type=tmpfs,target=/tmp,tmpfs-mode=1777,tmpfs-size=4GB \
+        --mount type=tmpfs,target=/tmp,tmpfs-mode=1777,tmpfs-size=$TMP_RAM \
         --platform linux/amd64 \
-        --cpus 8.0 --cpu-shares 1024 --memory 16GB --memory-reservation 2GB \
-        --name qt-multimedia-$(uuidgen)
-        docker.io/bensuperpc/multimedia:archlinux-base \
+        --cpus "$CPUS" --cpu-shares "$CPU_SHARES" --memory-reservation "$RES_RAM" --memory "$MAX_RAM" \
+        --name "$DOCKER_IMAGE" \
+        "$REGISTRY/$IMAGE:$TAG" \
         "$@"
+
+
+# Run the docker container
+# --device=/dev/dri \
