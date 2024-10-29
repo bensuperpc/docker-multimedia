84415345351939300a7406554c6a773134d16474
Bensuperpc <bensuperpc@gmail.com>
2024-10-29 22:42:45 +0100

    Improve scripts
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- test/cwebp/command_cwebp.sh
+++ test/cwebp/command_cwebp.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters: ${#}"
+    echo "Wrong number of parameters: ${#}, expected 2 parameters: <preset> <z>"
     exit 1
 fi
 
@@ -10,25 +10,17 @@ function convert_to_webp {
     local file="${1}"
     local preset="${2}"
     local z="${3}"
+
+    # Convert the image to WebP format
     cwebp -quiet -preset "${preset}" -metadata all -lossless -exact -z "${z}" "${file}" -o "${file%.*}_${preset}_z${z}.webp"
+    # Copy the timestamp from the original file
     touch -r "${file}" "${file%.*}_${preset}_z${z}.webp"
-    compare -metric AE "${file}" "${file%.*}_${preset}_z${z}.webp" null: || { echo "Error: images are different"; exit 1; }
-    diff -q <(magick "${file}" ppm:-) <(magick "${file%.*}_${preset}_z${z}.webp" ppm:-) || { echo "Error: images are different"; exit 1; }
+    # Check if the images are different
+    compare -metric AE "${file}" "${file%.*}_${preset}_z${z}.webp" null: || { echo "Error: images differ after compare check"; exit 1; }
+    diff -q <(magick "${file}" ppm:-) <(magick "${file%.*}_${preset}_z${z}.webp" ppm:-) || { echo "Error: images differ after diff check"; exit 1; }
 }
 
 export -f convert_to_webp
 
-find . -name "*.png" -print0 | parallel -0 -eta convert_to_webp "{}" "${1}" "${2}"
-
-# Disable -mt -progress
-# Convert to webp and keep exact same image bit by bit, error if not
-#find . -name "*.png" -print0 | parallel -0 -eta cwebp -quiet -preset ${1} -metadata all -lossless -exact -z ${2} "{}" -o "{.}_${1}_z${2}.webp"
-
-# Keep the same timestamp as the original image
-#find . -name "*.png" -exec sh -c 'touch -r "${0%.*}.png" "${0%.*}_${1}_z${2}.webp"' "{}" ${1} ${2} \;
-
-# Compare the original image with the webp image (compare)
-#find . -name "*.png" -exec sh -c 'compare -metric AE "${0%.*}.png" "${0%.*}_${1}_z${2}.webp" null: || { echo "Error: images are different"; exit 1; }' "{}" ${1} ${2} \;
-
-# Compare the original image with the webp image (diff)
-#find . -name "*.png" -exec sh -c 'diff -q <(magick "${0%.*}.png" ppm:-) <(magick "${0%.*}_${1}_z${2}.webp" ppm:-) || { echo "Error: images are different"; exit 1; }' "{}" ${1} ${2} \;
+# --progress --line-buffer --bar
+find . -name "*.png" -type f -print0 | parallel -0 convert_to_webp "{}" "${1}" "${2}"
--- test/cwebp/run_cwebp.sh
+++ test/cwebp/run_cwebp.sh
@@ -5,4 +5,17 @@ ARG1=${1:-preset_cwebp.txt}
 ARG2=${2:-z_webp.txt}
 CPU_CORES=${3:-1}
 
-parallel -j ${CPU_CORES} ./docker-multimedia.sh ./command_cwebp.sh {1} {2} :::: ${ARG1} ${ARG2}
+# Check if the files exist
+
+if [ ! -f "${ARG1}" ]; then
+    echo "Error: ${ARG1} does not exist"
+    exit 1
+fi
+
+if [ ! -f "${ARG2}" ]; then
+    echo "Error: ${ARG2} does not exist"
+    exit 1
+fi
+
+# --line-buffer
+parallel --jobs ${CPU_CORES} ./docker-multimedia.sh ./command_cwebp.sh {1} {2} :::: ${ARG1} ${ARG2}
