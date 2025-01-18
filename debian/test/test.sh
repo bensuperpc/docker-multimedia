77473bbbd74781cf24db620fa0598324b1dacfc1
Bensuperpc <bensuperpc@gmail.com>
2025-01-18 10:48:53 +0100

    Add packages
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- debian/test/test.sh
+++ debian/test/test.sh
@@ -6,13 +6,10 @@ set -euo pipefail
 ffmpeg -version
 echo "ffmpeg OK"
 
-mogrify -version
-echo "magick/mogrify OK"
-
 HandBrakeCLI --version
 echo "HandBrakeCLI OK"
 
-# Check image encoders/decoders
+# Check image encoders/decoders/tools
 cwebp -version
 echo "cwebp OK"
 
@@ -22,12 +19,28 @@ echo "avifenc OK"
 cjxl --version
 echo "cjxl OK"
 
-# Check video encoders/decoders
+magick -version
+echo "magick OK"
+
+# Check video encoders/decoders/tools
 SvtAv1EncApp --version
 echo "SvtAv1EncApp OK"
+
+rav1e --version
+echo "rav1e OK"
 
 x265 --version
 echo "x265 OK"
 
 x264 --version
 echo "x264 OK"
+
+# Check audio encoders/decoders/tools
+melt -version
+echo "melt OK"
+
+opusenc --version
+echo "opusenc OK"
+
+lame --version
+echo "lame OK"
