c565a6d0322ab4b84d4767fc42fff780f5f7e315
Bensuperpc <bensuperpc@gmail.com>
2025-02-11 19:58:53 +0100

    Add one more check on png to lossless webp converter

--- test/cwebp_png/convert_png_webp.sh
+++ test/cwebp_png/convert_png_webp.sh
@@ -7,25 +7,30 @@ readonly q=${2:-100}
 trap 'echo "An error occurred. Exiting." >&2; exit 1' ERR
 
 function convert_to_webp {
-    local file="$1"
-    local output_file="${file%.*}_z${z}_q${q}_lossless.webp"
+    local input_file="$1"
+    local output_file="${input_file%.*}_z${z}_q${q}_lossless.webp"
 
     # Convert the image to WebP format -mt
-    cwebp -quiet -metadata all -lossless -exact -z "$z" -q "$q" "$file" -o "$output_file"
+    cwebp -quiet -metadata all -lossless -exact -z "$z" -q "$q" "$input_file" -o "$output_file"
 
     # Copy the timestamp from the original file
-    touch -r "$file" "$output_file"
+    touch -r "$input_file" "$output_file"
 
     # Copy metadata from the original file (already copied with -metadata all)
-    #exiftool -TagsFromFile "$file" "$output_file"
+    #exiftool -TagsFromFile "$input_file" "$output_file"
 
     # Check if the images are identical
-    if ! compare -metric AE "$file" "$output_file" null: >/dev/null 2>&1; then
-        echo "Error: images differ for file $file" >&2
+    if ! compare -metric AE "$input_file" "$output_file" null: >/dev/null 2>&1; then
+        echo "Error: images differ for file $input_file" >&2
         exit 1
     fi
-    if ! diff -q <(magick "$file" ppm:-) <(magick "$output_file" ppm:-) >/dev/null 2>&1; then
-        echo "Error: images differ for file $file" >&2
+    if ! diff -q <(magick "$input_file" ppm:-) <(magick "$output_file" ppm:-) >/dev/null 2>&1; then
+        echo "Error: images differ for file $input_file" >&2
+        exit 1
+    fi
+    if ! [ "$(ffmpeg -hide_banner -loglevel error -i "$input_file" -f image2pipe -pix_fmt rgba -vcodec rawvideo - | sha256sum)" \
+            = "$(ffmpeg -hide_banner -loglevel error -i "$output_file" -f image2pipe -pix_fmt rgba -vcodec rawvideo - | sha256sum)" ] 2>&1; then
+        echo "Error: images differ for file $input_file" >&2
         exit 1
     fi
 }
