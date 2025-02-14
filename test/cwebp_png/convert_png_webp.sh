961e830c68858bf643285b841147979a843598f5
Bensuperpc <bensuperpc@gmail.com>
2025-02-14 23:26:04 +0100

    Update script

--- test/cwebp_png/convert_png_webp.sh
+++ test/cwebp_png/convert_png_webp.sh
@@ -9,6 +9,13 @@ trap 'echo "An error occurred. Exiting." >&2; exit 1' ERR
 function convert_to_webp {
     local input_file="$1"
     local output_file="${input_file%.*}_z${z}_q${q}_lossless.webp"
+    #local output_file="${input_file%.*}.webp"
+
+    if [ -f "$output_file" ]; then
+        echo "File $output_file already exists. Skipping." >&2
+        check_image_diff "$input_file" "$output_file"
+        return
+    fi
 
     # Convert the image to WebP format -mt
     cwebp -quiet -metadata all -lossless -exact -z "$z" -q "$q" "$input_file" -o "$output_file"
@@ -19,23 +26,33 @@ function convert_to_webp {
     # Copy metadata from the original file (already copied with -metadata all)
     #exiftool -TagsFromFile "$input_file" "$output_file"
 
+    check_image_diff "$input_file" "$output_file"
+}
+
+function check_image_diff {
+    local input_file="$1"
+    local output_file="$2"
+
     # Check if the images are identical
     if ! compare -metric AE "$input_file" "$output_file" null: >/dev/null 2>&1; then
         echo "Error: images differ for file $input_file" >&2
-        exit 1
+        rm -f "$output_file"
+        return
     fi
     if ! diff -q <(magick "$input_file" ppm:-) <(magick "$output_file" ppm:-) >/dev/null 2>&1; then
         echo "Error: images differ for file $input_file" >&2
-        exit 1
+        rm -f "$output_file"
+        return
     fi
-    if ! [ "$(ffmpeg -hide_banner -loglevel error -i "$input_file" -f image2pipe -pix_fmt rgba -vcodec rawvideo - | sha256sum)" \
-            = "$(ffmpeg -hide_banner -loglevel error -i "$output_file" -f image2pipe -pix_fmt rgba -vcodec rawvideo - | sha256sum)" ] 2>&1; then
+    if ! [ "$(ffmpeg -hide_banner -loglevel error -i "$input_file" -f image2pipe -pix_fmt rgba -vcodec rawvideo - | sha256sum 2>/dev/null)" \
+            = "$(ffmpeg -hide_banner -loglevel error -i "$output_file" -f image2pipe -pix_fmt rgba -vcodec rawvideo - | sha256sum 2>/dev/null)" ]; then
         echo "Error: images differ for file $input_file" >&2
-        exit 1
+        rm -f "$output_file"
+        return
     fi
 }
 
-export -f convert_to_webp
+export -f convert_to_webp check_image_diff
 export z q
 
 # --progress --line-buffer --bar --halt now,fail=1
